730b83c824ab80786107d765ca4865595c789976
Aditya Raj Verma <Adityav2410@gmail.com>
2017-10-03 22:07:43 -0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,9 +3,6 @@ IMAGE_WIDTH = 224
 IMAGE_HEIGHT = 224 
 NUM_CLASSES = 3
 
-# base_script_name = 'C:/Users/Chetan/Documents/CSE253/FinalProject/Place3/data/'#os.path.splitext(__file__)[0]
-# filepath=base_script_name + "1-{epoch:02d}-val_acc-{val_acc:.2f}.hdf5"
-
 base_dir = '/home/ubuntu/Nexar/nexar_traffic_light_train/'
 training_dir = '/train/'
 validation_dir = '/test/'
